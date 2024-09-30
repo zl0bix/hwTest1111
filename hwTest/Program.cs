13ec573212df7e9ea3e6a98429ed054c50e049ab
Andrew <zl0bix@icloud.com>
2024-09-30 13:52:53 +0500

    Добавьте файлы проекта.

---
+++ hwTest/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hwTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix1 = new int[4, 6];
+          
+            Random random = new Random();
+            string str = "" ;
+            Console.WriteLine(matrix1.GetLength(0));
+            //matrix1.Rank;
+            for (int i = 0; i < matrix1.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix1.GetLength(1); j++)
+                {
+                    matrix1[i,j] = random.Next(1, 11);
+                    Console.Write(matrix1[i, j]+"\t");
+                    
+                    if (j ==  matrix1.GetLength(1) -1 )
+                    {
+                        str += matrix1[i, j];
+                    }                                        
+                }
+                Console.WriteLine();               
+            }
+            Console.WriteLine(str);
+        }
+    }
+}
